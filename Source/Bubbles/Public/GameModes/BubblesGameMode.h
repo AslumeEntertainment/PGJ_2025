803b80be787dc947bcf00b0a4acb224fc224b52a
Ad04a <andrey.todorov04@gmail.com>
2025-01-26 06:58:31 +0200

    WTF

--- Source/Bubbles/Public/GameModes/BubblesGameMode.h
+++ Source/Bubbles/Public/GameModes/BubblesGameMode.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Headers/GeneralDelegates.h"
 #include "BubblesGameMode.generated.h"
+
+class ABubbleController;
+class AHumanBubble;
 
 UCLASS(minimalapi)
 class ABubblesGameMode : public AGameModeBase
@@ -12,7 +16,55 @@ class ABubblesGameMode : public AGameModeBase
 	GENERATED_BODY()
 
 public:
+
+	UPROPERTY(EditAnywhere)
+	int PlayersNeededForSession = 2;
+
+	UPROPERTY(VisibleAnywhere)
+	int ContaminateorTeam = -1;
+
+	UPROPERTY(VisibleAnywhere)
+	TMap<ABubbleController*, int> Players;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AHumanBubble> CleanerClass;
+
+	UPROPERTY(EditAnywhere)
+	FVector CleanerLocation;
+
+	UPROPERTY(EditAnywhere)
+	FRotator CleanerRotation;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AHumanBubble> ContaminatorClass;
+
+	UPROPERTY(EditAnywhere)
+	FVector ContaminatorLocation;
+
+	UPROPERTY(EditAnywhere)
+	FRotator ContaminatorRotation;
+
+	FTimerHandle PrepearingPeriodTimer;
+	float PrepearingPeriodLenght = 5;
+
+	FTimerHandle GamePeriodTimer;
+	float GamePeriodLenght = 300;
+
+	void CheckLobbyReadiness();
+
 	ABubblesGameMode();
+
+	void PostLogin(APlayerController* NewPlayer) override;
+
+	void PrepareGame();
+
+	void StartGame();
+
+	void Countdown();
+
+	void EndGame();
+
+	FTextTransferSignature OnLobbyMessegeChanged;
 };
 
 
